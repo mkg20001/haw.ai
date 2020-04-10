e51676cd3f665bb0e59145b4d31100d1ba63c2bb
Maciej Krüger <mkg20001@gmail.com>
2020-04-10 23:05:32 +0200

    feat: make it work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const fs = require('fs')
+const path = require('path')
 
 const init = async () => {
+  const stream = fs.createWriteStream(path.join(process.cwd(), 'raw.json'), { flags: 'a' })
+
   const server = Hapi.server({
     port: 3333,
     host: 'localhost'
@@ -21,7 +25,9 @@ const init = async () => {
 
   server.route({
     method: 'POST',
+    path: '/io',
     handler: async (request, h) => {
+      stream.write(JSON.stringify({ headers: request.raw.req.headers, payload: request.payload, date: Date.now() }) + '\n')
       return { text: 'Message Saved!' }
     }
   })
